a8562b7eeb31376ae4bfb09e91da675c1ac95ae4
HALUK BAYRAKCI <halukbayrakci32@gmail.com>
2023-06-28 20:54:10 +0300

    Initial Commit

---
+++ DataBaseWithListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataBaseWithListApp
+//
+//  Created by HALUK BAYRAKCI on 28.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
